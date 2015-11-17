7028c0bb9a356dee38301f2a2a7f398e5e5b53d3
Tyler Moody <tmoody@lclark.edu>
2015-11-16 18:16:31 -0800

    bleh

--- src/Producer.java
+++ src/Producer.java
@@ -3,5 +3,4 @@ public interface Producer {
 	int produce();//transforms resources into goods, returns number of goods
 	int getNumGoods();//gets number of goods produced
 	void calculateProfit();//calculates profit made by producer in time period
-
 }
